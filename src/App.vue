c9b8397c3207199624e9d7168dd243bc319127a1
daniel <daanduque@utp.edu.co>
2021-10-04 07:38:21 -0500

    before add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <the-navigation />
     <router-view/>
   </div>
 </template>
+
+<script>
+import TheNavigation from './components/TheNavigation.vue'
+export default {
+  components: { TheNavigation },
+  
+}
+</script>
 
 <style>
 #app {
@@ -17,16 +22,5 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
